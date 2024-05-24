e8667ddbfe47b8adc547f4ed4f2c50206bb75519
KIRILL <oniblekir@gmail.com>
2024-05-24 14:05:23 +0300

    добавил отрисовку 3 точек маршрута

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import FilterView from './view/filter.js';
 import SortView from './view/sort.js';
+import PointView from './view/event.js';
 import { render } from './render.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
@@ -8,3 +9,10 @@ render(new FilterView(), siteFilterElement);
 const siteSortElement = document.querySelector('.trip-events');
 render(new SortView(), siteSortElement);
 
+const siteEventEdit = siteSortElement;
+for(let i=0; i < 3; i++){
+    render(new PointView(), siteEventEdit);
+};
+
+// const sitePointElement = document.querySelector('.trip-events__list');
+// render(new PointView(), sitePointElement);
