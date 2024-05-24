8f14dc8ee18130d978929bddcd2cef2b5338f2cd
Dkey51 <oniblekir@gmail.com>
2024-05-24 19:27:44 +0300

    стала отображаться форма для точки маршрута

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import FilterView from './view/filter.js';
 import SortView from './view/sort.js';
 import PointView from './view/event.js';
-import FormView from '/view/event-form.js';
+import FormView from './view/event-form.js';
 import { render } from './render.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
@@ -10,10 +10,10 @@ render(new FilterView(), siteFilterElement);
 const siteSortElement = document.querySelector('.trip-events');
 render(new SortView(), siteSortElement);
 
+const siteEventFormEdit = document.querySelector('.trip-events');
+render(new FormView(), siteEventFormEdit);
+
 const siteEventEdit = siteSortElement;
 for(let i = 0; i < 3; i++){
   render(new PointView(), siteEventEdit);
 }
-
-const siteEventFormEdit = document.querySelector('.trip-events');
-render(new FormView(), siteEventFormEdit);
