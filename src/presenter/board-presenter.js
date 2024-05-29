d1cf81755f05fb2623fc03a7ae753b3b54baba02
Dkey51 <oniblekir@gmail.com>
2024-05-29 19:14:13 +0300

    доделать

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,11 +1,11 @@
-import FilterView from '../view/filter.js';
+// import FilterView from '../view/filter.js';
 import SortView from '../view/sort.js';
 import PointView from '../view//event.js';
 import FormView from '../view//event-form.js';
 import {render} from '../render.js';
 
 export default class BoardPresenter {
-  filterComponent = new FilterView();
+  // filterComponent = new FilterView();
   sortComponent = new SortView();
   formComponent = new FormView();
   pointComponent = new PointView();
@@ -15,8 +15,13 @@ export default class BoardPresenter {
   }
 
   init() {
-    render(this.filterComponent, this.container);
-    render(this.sortComponent, this.sortComponent);
+    //render(this.filterComponent, this.container);
+    render(this.sortComponent, this.container);
+    for (let i = 0 ; i < 3; i++){
+      render(this.pointComponent, this.container);
+
+    }
+    render(this.formComponent, this.container);
 
   }
 
