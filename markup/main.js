a26a85cae1e80dae5683c61eafcd68a51c9c0060
Dkey51 <oniblekir@gmail.com>
2024-06-17 22:02:48 +0300

    откатил изменения

--- markup/main.js
+++ markup/main.js
@@ -1,13 +1,18 @@
 
-import BoardPresenter from './presenter/board-presenter.js';
-import FilterView from './view/filter.js';
-import { render } from './render.js';
+import BoardPresenter from '../src/presenter/board-presenter.js';
+import FilterView from '../src/view/filter.js';
+import { render } from '../src/render.js';
+import PointModel from '../src/module/model.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 render(new FilterView(), siteFilterElement);
 
 const siteMainElement = document.querySelector('.trip-events');
 
-const boardPresenter = new BoardPresenter({container: siteMainElement});
+const pointModel = new PointModel();
+const boardPresenter = new BoardPresenter({
+  container: siteMainElement,
+  pointModel,
+});
 
 boardPresenter.init();
