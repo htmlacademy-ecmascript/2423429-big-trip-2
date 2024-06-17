2f298342365f4a7aaf4318fce0fb450c065437b9
Dkey51 <oniblekir@gmail.com>
2024-06-17 19:19:56 +0300

    внес правки по PR

--- src/view/main.js
+++ src/view/main.js
@@ -1,15 +1,14 @@
 
-import BoardPresenter from './presenter/board-presenter.js';
-import FilterView from './view/filter.js';
-import { render } from './render.js';
-import PointModel from './module/model.js';
+import BoardPresenter from '../presenter/board-presenter.js';
+import FilterView from './filter.js';
+import { render } from '../render.js';
+import PointModel from '../module/model.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 render(new FilterView(), siteFilterElement);
 
 const siteMainElement = document.querySelector('.trip-events');
 
-//const boardPresenter = new BoardPresenter({container: siteMainElement});
 const pointModel = new PointModel();
 const boardPresenter = new BoardPresenter({
   container: siteMainElement,
