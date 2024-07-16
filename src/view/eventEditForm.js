2959245d770cccdbdb95c000e411ceb26619e77f
Dkey51 <oniblekir@gmail.com>
2024-07-16 11:48:44 +0300

    убрал формат у иконок

--- src/view/eventEditForm.js
+++ src/view/eventEditForm.js
@@ -8,7 +8,7 @@ function createEditor(point){
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/${point.type}.png" alt="Event type icon">
+          <img class="event__type-icon" width="17" height="17" src="img/icons/${point.type}" alt="Event type icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
