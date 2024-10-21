c05c7377b80c54bbbc8f3f1912651882c7a164b9
KIRILL <oniblekir@gmail.com>
2024-10-21 15:02:27 +0300

    8.1.2 Добавит модуль PointsApiService в точке входа. WIP - Добавим импорт модуля - Создадим экземпляр, передадим его в модель задач и попробуем получить задачи с сервера - Выведем результат в консоль
    WIP: линтер ругается на консоль

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import BoardPresenter from './presenter/board-presenter.js';
 import FilterView from './view/filter.js';
-
 import { render } from './framework/render.js';
 import PointModel from './model/point-model.js';
 import OffersModel from './model/offer-model.js';
 import CitiesModel from './model/cities-model.js';
+import PointsApiService from './points-api-service.js';
+
+const AUTHORIZATION = 'Basic iJ5mgI78cvo4oi5i';
+const END_POINT = 'https://22.objects.htmlacademy.pro/big-trip';
 
 const siteMainElement = document.querySelector('.trip-events');
 const siteMainTripEvent = document.querySelector('.main-button');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 
-const pointModel = new PointModel();
+const pointModel = new PointModel({
+  pointsApiService: new PointsApiService(END_POINT, AUTHORIZATION)
+});
+
 const offersModel = new OffersModel();
 const citiesModel = new CitiesModel();
 const boardPresenter = new BoardPresenter({
@@ -20,19 +26,6 @@ const boardPresenter = new BoardPresenter({
   offersModel,
   citiesModel,
 });
-
-// const newPointButtonComponent = new NewPointButtonView ({
-//   onClick: handleNewPointButtonClick
-// });
-
-// function handleNewPointFormClose() {
-//   newPointButtonComponent.disabled = false;
-// }
-
-// function handleNewPointButtonClick() {
-//   boardPresenter.createPoint();
-//   newPointButtonComponent.disabled = true;
-// }
 
 render(new FilterView(), siteFilterElement);
 
